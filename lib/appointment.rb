fe85251e7ea283fa8eee54aba80609896460d8fa
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-02 21:52:17 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -6,7 +6,7 @@ class Appointment
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
-    @doctor.add_appointment(date)
+    @doctor.add_appointment(self)
   end
 
   #def add_appointment()
