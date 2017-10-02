e0e6081cc4c19d79a49267f88f566b67175463a3
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-02 21:50:22 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,6 +4,7 @@ class Appointment
   attr_accessor :patient, :doctor
   attr_reader :name
   def initialize(date, doctor)
+    binding.pry
     @date = date
     @doctor = doctor
     @doctor.add_appointment(date)
