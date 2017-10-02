033b141b089e1a093ba6a69f90e203866c34bc69
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-02 21:19:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
 
-  attr_reader :name, :genre
-  attr_accessor :artist
+  attr_reader :name, :genre, :artist
   def initialize(name, genre)
     @name = name
     @genre = genre
     @genre.songs << self
+    @genre.artists << self.artist
   end
 
 
